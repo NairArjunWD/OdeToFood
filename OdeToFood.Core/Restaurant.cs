fe60efcf2dfefeda03bc02db20b2ff80b391b6f2
Arjun Nair <nair.arjun47@yahoo.com>
2020-05-19 23:15:52 -0700

    Move cuisine and restaurant files into core dir. Create data file and create new cs file.

--- OdeToFood.Core/Restaurant.cs
+++ OdeToFood.Core/Restaurant.cs
@@ -9,5 +9,6 @@ namespace OdeToFood.Core
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
+        public CruisineType Cuisine { get; set; }
     }
 }
---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    interface IRestaurantData
+    {
+        
+    }
+}
