e083dee9ed9bd72466e767f72de21a9d6f7c9a3e
Arjun Nair <nair.arjun47@yahoo.com>
2020-05-19 23:29:41 -0700

    Add restaurants

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -12,6 +12,17 @@ namespace OdeToFood.Data
 
     public class InMemoryRestaurantData : IRestaurantData
     {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Sikh Palace", Location = "London", Cuisine = CuisineType.Indian},
+                new Restaurant {Id = 3, Name = "Lopez's", Location = "California", Cuisine = CuisineType.Mexican}
+            };
+        }
+
         public IEnumerable<Restaurant> GetAll()
         {
             throw new NotImplementedException();
